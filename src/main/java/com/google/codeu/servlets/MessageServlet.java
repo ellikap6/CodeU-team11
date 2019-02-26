d8df06054fbd72c2f8dcc230f669f26e395dc00f
jussepen <jussepen@gmail.com>
2019-02-25 20:36:19 -0500

    Datastore,Message,MessageServlet

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -77,10 +77,12 @@ public class MessageServlet extends HttpServlet {
 
     String user = userService.getCurrentUser().getEmail();
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String recipient = request.getParameter("recipient"); 
 
-    Message message = new Message(user, text);
-    datastore.storeMessage(message);
 
-    response.sendRedirect("/user-page.html?user=" + user);
+    Message message = new Message(user, text, recipient);
+    datastore.storeMessage(message, Datastore);
+
+    response.sendRedirect("/user-page.html?user=" + recipient);
   }
 }
