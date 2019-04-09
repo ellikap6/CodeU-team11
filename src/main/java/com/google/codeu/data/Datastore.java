f33fddfa2f533ab76299b5514ddafdf399b930f5
Tofe Salako <tsalako12@gmail.com>
2019-04-09 15:39:04 -0700

    All the objects we should need for open project (psuedo defined in datastore, still needs to be defined in actual classes.

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -37,67 +37,122 @@ public class Datastore {
     datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
-  /** Returns the total number of messages for all users. */
-  public int getTotalMessageCount(){
-    Query query = new Query("Message");
-    PreparedQuery results = datastore.prepare(query);
-    return results.countEntities(FetchOptions.Builder.withLimit(1000));
-  }
-
-  /** Stores the Message in Datastore. */
-  public void storeMessage(Message message) {
-    Entity messageEntity = new Entity("Message", message.getId().toString());
-    messageEntity.setProperty("user", message.getUser());
-    messageEntity.setProperty("text", message.getText());
-    messageEntity.setProperty("timestamp", message.getTimestamp());
-    messageEntity.setProperty("recipient", message.getRecipient());
-    messageEntity.setProperty("sentimentScore", message.getSentimentScore());
-
-    if(message.getImageUrl() != null) {
-      messageEntity.setProperty("imageUrl", message.getImageUrl());
-    }
-    datastore.put(messageEntity);
-  }
-
-  /**
-   * Gets messages posted by a specific user.
-   *
-   * @param String user's name
-   * @return a list of messages posted by the user, or empty list if user has never posted a
-   *     message. List is sorted by time descending.
-   */
-  public List<Message> getMessages(String recipient) {
-	  Query query = new Query("Message")
-		            .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
-		            .addSort("timestamp", SortDirection.DESCENDING);
-	  PreparedQuery results = datastore.prepare(query);
-
-      return fillMessageList(results);
-  }
-
-  /**
-   * Gets all messages posted.
-   *
-   * @return a list of all messages posted, or empty list if there are no posted
-   *     messages. List is sorted by time descending.
-   */
-  public List<Message> getAllMessages(){
-	  Query query = new Query("Message")
+  public List<Post> getAllPosts() {
+    Query query = new Query("Post")
 			    .addSort("timestamp", SortDirection.DESCENDING);
 	  PreparedQuery results = datastore.prepare(query);
 
-	  return fillMessageList(results);
+    List<Post> posts = new ArrayList<>();
 
-	 }
+		for (Entity entity : results.asIterable()) {
+			try {
+        posts.add(buildPost(entity));
+			} catch (Exception e) {
+				System.err.println("Error reading message.");
+				System.err.println(entity.toString());
+				e.printStackTrace();
+			}
+		}
+		return posts;
+  }
 
-	/**
-	 * Private helper method to create encapsulation for getMessages() and
-	 * getAllMessages(). Takes a given query that specifies sorting and filtering.
-	 *
-	 * @param results a PreparedQuery that specifies sorting and filtering of the messages
-	 * @return List of messages from the posted messages, filtered and sorted
-	 */
-	private List<Message> fillMessageList(PreparedQuery results) {
+  public Post getPost(UUID postID) {
+    Query query = new Query("Post")
+                  .setFilter(new Query.FilterPredicate(
+                                  "postID",
+                                  FilterOperator.EQUAL,
+                                  postID.toString()));
+    PreparedQuery results = datastore.prepare(query);
+
+    Entity entity = results.asSingleEntity();
+	  if(entity == null) {
+	   return null;
+	  }
+
+    return buildPost(entity);
+  }
+
+  public Post buildPost(Entity entity) {
+    String idString = entity.getKey().getName();
+    UUID postID = UUID.fromString(idString);
+    String creator = (String) entity.getProperty("creator");
+    String coverImageUrl = (String) entity.getProperty("coverImageUrl");
+    long timestamp = (long) entity.getProperty("timestamp");
+
+    Marker marker = getMarker(postID);
+    List<ChartDataRow> chartDataRows = getChartDataRows(postID);
+    List<Message> messages = getMessages(postID);
+
+    return new Post(
+      postID,
+      creator,
+      timestamp,
+      coverImageUrl,
+      marker,
+      chartData,
+      messages);
+  }
+
+  public void storePost(Post post) {
+    Entity postEntity = new Entity("ChartData", post.getId().toString());
+    postEntity.setProperty("creator", post.getCreator());
+    postEntity.setProperty("timestamp", post.getTimestamp());
+    postEntity.setProperty("coverImageUrl", post.getCoverImageUrl());
+    for (ChartDataRow row : post.getChartDataRows()) {
+      storeChartDataRow(row);
+    }
+    storeMarker(post.getMarker());
+
+    // Messages are not stored up front. They will be stored on the post page
+    // with storeMessage() and they will have to postID to link back to this
+    // post.
+    datastore.put(postEntity);
+  }
+
+  public List<ChartDataRow> getChartDataRows(UUID postID) {
+    Query query = new Query("ChartData")
+                  .setFilter(new Query.FilterPredicate(
+                                  "postID",
+                                  FilterOperator.EQUAL,
+                                  postID.toString()));
+    PreparedQuery results = datastore.prepare(query);
+
+    List<ChartDataRow> chartDataRows = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+			try {
+        UUID postID = UUID.fromString((String) entity.getProperty("postID"));
+        long date = (long) entity.getProperty("date");
+        int value = (int) entity.getProperty("value");
+
+				ChartDataRow chartDataRow = new ChartDataRow(date, value, postID);
+				chartDataRows.add(chartDataRow);
+			} catch (Exception e) {
+				System.err.println("Error reading message.");
+				System.err.println(entity.toString());
+				e.printStackTrace();
+			}
+		}
+    return chartDataRows;
+  }
+
+  public void storeChartDataRow(ChartDataRow chartDataRow) {
+    Entity chartDataRowEntity = new Entity("ChartData");
+    chartDataRowEntity.setProperty("date", chartDataRow.getDate());
+    chartDataRowEntity.setProperty("value", chartDataRow.getValue());
+    chartDataRowEntity.setProperty("postID", chartDataRow.getPostID().toString());
+    datastore.put(chartDataRowEntity);
+  }
+
+  public List<Message> getMessages(UUID postID) {
+	  Query query = new Query("Message")
+		            .setFilter(new Query.FilterPredicate("postID", FilterOperator.EQUAL, postID.toString()))
+		            .addSort("timestamp", SortDirection.DESCENDING);
+	  PreparedQuery results = datastore.prepare(query);
+
+    return fillMessageList(results);
+  }
+
+  private List<Message> fillMessageList(PreparedQuery results) {
 		List<Message> messages = new ArrayList<>();
 
 		for (Entity entity : results.asIterable()) {
@@ -106,13 +161,13 @@ public class Datastore {
 				UUID id = UUID.fromString(idString);
 				String user = (String) entity.getProperty("user");
 				String text = (String) entity.getProperty("text");
-        String recipient = (String) entity.getProperty("recipient");
+        UUID postID = UUID.fromString((String) entity.getProperty("postID"));
 				long timestamp = (long) entity.getProperty("timestamp");
 				float sentimentScore = entity.getProperty("sentimentScore") == null? (float) 0.0
 						: ((Double) entity.getProperty("sentimentScore")).floatValue();
 
         String imageUrl = (String) entity.getProperty("imageUrl");
-				Message message = new Message(id, user, text, timestamp, sentimentScore,recipient, imageUrl);
+				Message message = new Message(id, user, text, timestamp, sentimentScore, postID, imageUrl);
 				messages.add(message);
 			} catch (Exception e) {
 				System.err.println("Error reading message.");
@@ -123,72 +178,47 @@ public class Datastore {
 		return messages;
   }
 
-	/** Stores the User in Datastore. */
-	 public void storeUser(User user) {
-	  Entity userEntity = new Entity("User", user.getEmail());
-	  userEntity.setProperty("email", user.getEmail());
-	  userEntity.setProperty("aboutMe", user.getAboutMe());
-	  datastore.put(userEntity);
-	 }
+  public void storeMessage(Message message) {
+    Entity messageEntity = new Entity("Message", message.getId().toString());
+    messageEntity.setProperty("user", message.getUser());
+    messageEntity.setProperty("text", message.getText());
+    messageEntity.setProperty("timestamp", message.getTimestamp());
+    messageEntity.setProperty("postID", message.getPostID().toString());
+    messageEntity.setProperty("sentimentScore", message.getSentimentScore());
 
-	 /**
-	  * Returns the User owned by the email address, or
-	  * null if no matching User was found.
-	  */
-	 public User getUser(String email) {
+    if(message.getImageUrl() != null) {
+      messageEntity.setProperty("imageUrl", message.getImageUrl());
+    }
+    datastore.put(messageEntity);
+  }
 
-	  Query query = new Query("User")
-	    .setFilter(new Query.FilterPredicate("email", FilterOperator.EQUAL, email));
-	  PreparedQuery results = datastore.prepare(query);
-	  Entity userEntity = results.asSingleEntity();
-	  if(userEntity == null) {
-	   return null;
-	  }
+  public Marker getMarker(UUID postID) {
+    Query query = new Query("Marker")
+                  .setFilter(new Query.FilterPredicate(
+                                  "postID",
+                                  FilterOperator.EQUAL,
+                                  postID.toString()));
+    PreparedQuery results = datastore.prepare(query);
 
-	  String aboutMe = (String) userEntity.getProperty("aboutMe");
-	  User user = new User(email, aboutMe);
+    Entity entity = results.asSingleEntity();
+    if(entity == null) {
+     return null;
+    }
 
-	  return user;
-	 }
-
-
-
-
-
-    public List<UserMarker> getMarkers() {
-    List<UserMarker> markers = new ArrayList<>();
-
-  Query query = new Query("UserMarker");
-  PreparedQuery results = datastore.prepare(query);
-
-  for (Entity entity : results.asIterable()) {
-   try {
     double lat = (double) entity.getProperty("lat");
     double lng = (double) entity.getProperty("lng");
     String content = (String) entity.getProperty("content");
-
-    UserMarker marker = new UserMarker(lat, lng, content);
-    markers.add(marker);
-   } catch (Exception e) {
-    System.err.println("Error reading marker.");
-    System.err.println(entity.toString());
-    e.printStackTrace();
-   }
+    UUID postID = UUID.fromString((String) entity.getProperty("postID"));
+    Marker marker = new Marker(lat, lng, content, postID);
+    return marker;
   }
-  return markers;
-}
 
-public void storeMarker(UserMarker marker) {
-  Entity markerEntity = new Entity("UserMarker");
-  markerEntity.setProperty("lat", marker.getLat());
-  markerEntity.setProperty("lng", marker.getLng());
-  markerEntity.setProperty("content", marker.getContent());
-  datastore.put(markerEntity);
-}
-
-
-
-
-
-
+  public void storeMarker(Marker marker) {
+    Entity markerEntity = new Entity("Marker");
+    markerEntity.setProperty("lat", marker.getLat());
+    markerEntity.setProperty("lng", marker.getLng());
+    markerEntity.setProperty("content", marker.getContent());
+    markerEntity.setProperty("postID",marker.getPostID().toString());
+    datastore.put(markerEntity);
+  }
 }
