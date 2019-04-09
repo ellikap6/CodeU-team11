7b589ba2df79952717ad43e74f18e815220a5e82
Tofe Salako <tsalako12@gmail.com>
2019-04-09 11:50:25 -0700

    charts 2

---
+++ src/main/webapp/js/chart-loader.js
@@ -0,0 +1,38 @@
+function drawChart(table){
+  var book_data = table;
+
+  var chart_options = {
+                      width: 800,
+                      height: 400
+        };
+  var chart = new google.visualization.BarChart(document.getElementById('message_chart'));
+  chart.draw(book_data, chart_options);
+}
+
+function fetchMessageData() {
+  fetch("/messagechart")
+      .then((response) => {
+        return response.json();
+      })
+      .then((msgJson) => {
+        var msgData = new google.visualization.DataTable();
+        //define columns for the DataTable instance
+        msgData.addColumn('date', 'Date');
+        msgData.addColumn('number', 'Message Count');
+        // console.log(msgJson);
+
+        for (i = 0; i < msgJson.length; i++) {
+            msgRow = [];
+            var timestampAsDate = new Date (msgJson[i].timestamp);
+            // var newDate = new Date(timestampAsDate.getYear(), timestampAsDate.getMonth(), timestampAsDate.getDay());
+            var totalMessages = i + 1;
+            //TODO add the formatted values to msgRow array by using JS' push method
+            msgRow.push(timestampAsDate);
+            msgRow.push(totalMessages);
+            // console.log(msgRow);
+            msgData.addRow(msgRow);
+        }
+        // console.log(msgData);
+        drawChart(msgData);
+      });
+}
